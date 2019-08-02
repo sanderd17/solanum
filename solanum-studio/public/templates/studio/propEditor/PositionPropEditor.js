5673ff6e01db44c3f8e177f38e30cfb30b7d0de7
Sander Deryckere <sanderd17@gmail.com>
2019-08-02 11:42:40 +0000

    Split position props off into separate component

---
+++ solanum-studio/public/templates/studio/propEditor/PositionPropEditor.js
@@ -0,0 +1,172 @@
+import Template from "/lib/template.js"
+import Checkbox from '/templates/forms/Checkbox.js'
+import Textbox from '/templates/forms/Textbox.js'
+
+class PositionPropEditor extends Template {
+
+    static defaultSize = [300, 150]
+    static childDefinitions  = {
+        leftActive: {
+            type: Checkbox,
+            position: { left: '0px', top: '0px', height: '20px', width: '100px' },
+            props: {
+                text: 'Left',
+                disabled: true
+            },
+            eventHandlers: {},
+            styles: []
+        },
+        rightActive: {
+            type: Checkbox,
+            position: { left: '0px', top: '25px', height: '20px', width: '100px' },
+            props: {
+                text: 'Right',
+                disabled: true
+            },
+            eventHandlers: {},
+            styles: []
+        },
+        widthActive: {
+            type: Checkbox,
+            position: { left: '0px', top: '50px', height: '20px', width: '100px' },
+            props: {
+                text: 'Width',
+                disabled: true
+            },
+            eventHandlers: {},
+            styles: []
+        },
+        topActive: {
+            type: Checkbox,
+            position: { left: '0px', top: '75px', height: '20px', width: '100px' },
+            props: {
+                text: 'Top',
+                disabled: true
+            },
+            eventHandlers: {},
+            styles: []
+        },
+        bottomActive: {
+            type: Checkbox,
+            position: { left: '0px', top: '100px', height: '20px', width: '100px' },
+            props: {
+                text: 'Bottom',
+                disabled: true
+            },
+            eventHandlers: {},
+            styles: []
+        },
+        heightActive: {
+            type: Checkbox,
+            position: { left: '0px', top: '125px', height: '20px', width: '100px' },
+            props: {
+                text: 'Height',
+                disabled: true
+            },
+            eventHandlers: {},
+            styles: []
+        },
+        leftValue: {
+            type: Textbox,
+            position: { left: '120px', top: '0px', height: '20px', right: '0px' },
+            props: { disabled: true },
+            eventHandlers: { change: (ev, root, child) => root.setPositionValue(child, 'left') },
+            styles: []
+        },
+        rightValue: {
+            type: Textbox,
+            position: { left: '120px', top: '25px', height: '20px', right: '0px' },
+            props: { disabled: true },
+            eventHandlers: { change: (ev, root, child) => root.setPositionValue(child, 'right') },
+            styles: []
+        },
+        widthValue: {
+            type: Textbox,
+            position: { left: '120px', top: '50px', height: '20px', right: '0px' },
+            props: { disabled: true },
+            eventHandlers: { change: (ev, root, child) => root.setPositionValue(child, 'width') },
+            styles: []
+        },
+        topValue: {
+            type: Textbox,
+            position: { left: '120px', top: '75px', height: '20px', right: '0px' },
+            props: { disabled: true },
+            eventHandlers: { change: (ev, root, child) => root.setPositionValue(child, 'top') },
+            styles: []
+        },
+        bottomValue: {
+            type: Textbox,
+            position: { left: '120px', top: '100px', height: '20px', right: '0px' },
+            props: { disabled: true },
+            eventHandlers: { change: (ev, root, child) => root.setPositionValue(child, 'bottom') },
+            styles: []
+        },
+        heightValue: {
+            type: Textbox,
+            position: {
+                left: "120px",
+                top: "125px",
+                height: "20px",
+                right: "0px"
+            },
+            props: { disabled: true },
+            eventHandlers: { change: (ev, root, child) => root.setPositionValue(child, 'height') },
+            styles: []
+        }
+
+    }
+
+    /**
+     * 
+     * @param {Textbox} child 
+     * @param {stirng} type 
+     */
+    async setPositionValue(textBox, type) {
+        for (let [childId, child] of Object.entries(this.cmpSelection)) {
+            let newPosition = {...child.position}
+            newPosition[type] = textBox.value
+            this.dom.dispatchEvent(new CustomEvent('positionpropchanged', {
+                bubbles: true,
+                detail: {childId, newPosition}
+            }))
+        }
+    }
+
+    recalcPositionParameters() {
+        for (let p of ['left', 'right', 'width', 'top', 'bottom', 'height']) {
+            let enabled = false
+            let text = null
+            for (let cmp of Object.values(this.cmpSelection)) {
+                if (p in cmp.position) {
+                    if (text == null || text == cmp.position[p]) {
+                        text = cmp.position[p]
+                    } else {
+                        text = ''
+                    }
+                    enabled = true
+                } else {
+                    enabled = false
+                }
+            }
+            this.children[p + 'Value'].disabled = !enabled
+            this.children[p + 'Value'].value = text || ''
+            this.children[p + 'Active'].checked = enabled
+            this.children[p + 'Active'].disabled = !enabled
+        }
+    }
+
+    /**
+     * @type {Object<string, Template>}
+     */
+    _cmpSelection = []
+    set cmpSelection(cmpSelection) {
+        this._cmpSelection = cmpSelection
+        this.recalcPositionParameters()
+    }
+
+    get cmpSelection() {
+        return this._cmpSelection
+    }
+}
+
+export default PositionPropEditor
--- solanum-studio/public/templates/studio/propEditor/PropEditor.js
+++ solanum-studio/public/templates/studio/propEditor/PropEditor.js
@@ -1,158 +1,18 @@
 import Template from "/lib/template.js"
-import Checkbox from '/templates/forms/Checkbox.js'
-import Textbox from '/templates/forms/Textbox.js'
+import PositionPropEditor from '/templates/studio/propEditor/PositionPropEditor.js'
 
 class PropEditor extends Template {
 
     static defaultSize = [300, 1000]
     static childDefinitions  = {
-        leftActive: {
-            type: Checkbox,
-            position: { left: '0px', top: '0px', height: '20px', width: '100px' },
-            props: {
-                text: 'Left',
-                disabled: true
-            },
-            eventHandlers: {},
-            styles: []
+        positionPropEditor: {
+            type: PositionPropEditor,
+            position: {left: '0px', right: '0px', top: '0px', height: '150px'}
         },
-        rightActive: {
-            type: Checkbox,
-            position: { left: '0px', top: '25px', height: '20px', width: '100px' },
-            props: {
-                text: 'Right',
-                disabled: true
-            },
-            eventHandlers: {},
-            styles: []
-        },
-        widthActive: {
-            type: Checkbox,
-            position: { left: '0px', top: '50px', height: '20px', width: '100px' },
-            props: {
-                text: 'Width',
-                disabled: true
-            },
-            eventHandlers: {},
-            styles: []
-        },
-        topActive: {
-            type: Checkbox,
-            position: { left: '0px', top: '75px', height: '20px', width: '100px' },
-            props: {
-                text: 'Top',
-                disabled: true
-            },
-            eventHandlers: {},
-            styles: []
-        },
-        bottomActive: {
-            type: Checkbox,
-            position: { left: '0px', top: '100px', height: '20px', width: '100px' },
-            props: {
-                text: 'Bottom',
-                disabled: true
-            },
-            eventHandlers: {},
-            styles: []
-        },
-        heightActive: {
-            type: Checkbox,
-            position: { left: '0px', top: '125px', height: '20px', width: '100px' },
-            props: {
-                text: 'Height',
-                disabled: true
-            },
-            eventHandlers: {},
-            styles: []
-        },
-        leftValue: {
-            type: Textbox,
-            position: { left: '120px', top: '0px', height: '20px', right: '0px' },
-            props: { disabled: true },
-            eventHandlers: { change: (ev, root, child) => root.setPositionValue(child, 'left') },
-            styles: []
-        },
-        rightValue: {
-            type: Textbox,
-            position: { left: '120px', top: '25px', height: '20px', right: '0px' },
-            props: { disabled: true },
-            eventHandlers: { change: (ev, root, child) => root.setPositionValue(child, 'right') },
-            styles: []
-        },
-        widthValue: {
-            type: Textbox,
-            position: { left: '120px', top: '50px', height: '20px', right: '0px' },
-            props: { disabled: true },
-            eventHandlers: { change: (ev, root, child) => root.setPositionValue(child, 'width') },
-            styles: []
-        },
-        topValue: {
-            type: Textbox,
-            position: { left: '120px', top: '75px', height: '20px', right: '0px' },
-            props: { disabled: true },
-            eventHandlers: { change: (ev, root, child) => root.setPositionValue(child, 'top') },
-            styles: []
-        },
-        bottomValue: {
-            type: Textbox,
-            position: { left: '120px', top: '100px', height: '20px', right: '0px' },
-            props: { disabled: true },
-            eventHandlers: { change: (ev, root, child) => root.setPositionValue(child, 'bottom') },
-            styles: []
-        },
-        heightValue: {
-            type: Textbox,
-            position: {
-                left: "120px",
-                top: "125px",
-                height: "20px",
-                right: "0px"
-            },
-            props: { disabled: true },
-            eventHandlers: { change: (ev, root, child) => root.setPositionValue(child, 'height') },
-            styles: []
-        }
-
-    }
-
-    /**
-     * 
-     * @param {Textbox} child 
-     * @param {stirng} type 
-     */
-    async setPositionValue(textBox, type) {
-        for (let [childId, child] of Object.entries(this.cmpSelection)) {
-            let newPosition = {...child.position}
-            newPosition[type] = textBox.value
-            this.dom.dispatchEvent(new CustomEvent('positionpropchanged', {
-                bubbles: true,
-                detail: {childId, newPosition}
-            }))
-        }
     }
 
     recalcPositionParameters() {
-        for (let p of ['left', 'right', 'width', 'top', 'bottom', 'height']) {
-            let enabled = false
-            let text = null
-            for (let cmp of Object.values(this.cmpSelection)) {
-                if (p in cmp.position) {
-                    if (text == null || text == cmp.position[p]) {
-                        text = cmp.position[p]
-                    } else {
-                        text = ''
-                    }
-                    enabled = true
-                } else {
-                    enabled = false
-                }
-            }
-            this.children[p + 'Value'].disabled = !enabled
-            this.children[p + 'Value'].value = text || ''
-            this.children[p + 'Active'].checked = enabled
-            this.children[p + 'Active'].disabled = !enabled
-        }
+        this.children.positionPropEditor.recalcPositionParameters()
     }
 
     /**
@@ -161,7 +21,7 @@ class PropEditor extends Template {
     _cmpSelection = []
     set cmpSelection(cmpSelection) {
         this._cmpSelection = cmpSelection
-        this.recalcPositionParameters()
+        this.children.positionPropEditor.cmpSelection = cmpSelection
     }
 
     get cmpSelection() {
