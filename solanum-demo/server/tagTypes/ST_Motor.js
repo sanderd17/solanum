2eb6738f0f0665e0d8494b3fa6a9be1f541debe4
Sander Deryckere <sanderd17@gmail.com>
2020-05-03 12:39:56 +0200

    Fix package lock to upgraded packages

---
+++ solanum-demo/server/tagTypes/ST_Motor.js
@@ -0,0 +1,27 @@
+import TagFolder from 'solanum-core/server/TagFolder.js'
+import MemoryTag from 'solanum-core/server/MemoryTag.js'
+
+class ST_Motor extends TagFolder {
+    constructor(tagset, tagpath, data) {
+        super(tagset, tagpath, {
+            sId: {
+                type: MemoryTag,
+                defaultValue: data.sId
+            },
+            bFwdMan: {
+                type: MemoryTag,
+                defaultValue: false
+            },
+            bRvsMan: {
+                type: MemoryTag,
+                defaultValue: false
+            },
+            sColor: {
+                type: MemoryTag,
+            defaultValue: `hsl(${(data.i) % 360}, 100%, 50%)`,
+            }
+        })
+    }
+}
+
+export default ST_Motor
